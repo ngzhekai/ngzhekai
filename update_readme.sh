7a1125ce20206bfc3b96a394a4e2c5b1362d96b9
Gilbert <ngzhekai@gmail.com>
2023-02-09 23:59:41 +0800

    Update update_readme.sh

--- update_readme.sh
+++ update_readme.sh
@@ -20,14 +20,13 @@ output="Howdy! 👋 I am *Gilbert Ng*（JP: ぎるばって呼んでいいよ �
 blank_space=""
 quote="### **<q>${quote}</q>** -<em>${author}</em>"
 note="> Updated on $dates - (UTC) $time"
-status="### Status [自分の情報]✅"
+status="### My Status [自分の情報]✅"
 # status_1="- 🙍‍♂️ Data Communication & Networking Student (Multimedia University Melaka)"
-status_2="- 🌱 I’m currently learning **about life**"
-stats="| ![Most Used Languages](https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api/top-langs/?username=ngzhekai&layout=compact&theme=material-palenight&bg_color=00000000&hide_border=true&custom_title=Most+Used+Programming+Languages) | ![GitHub Stats](https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api?username=ngzhekai&show_icons=true&count_private=true&theme=material-palenight&bg_color=00000000&hide_border=true&line_height=33&custom_title=Zhe+Kai's+GitHub+Statistics) |"
-stats_1="|:-----:|:-----:|"
+# status_2="- 🌱 I’m currently learning **about life**"
 stats_2="![](https://www.codewars.com/users/Giruba/badges/large)"
 stats_3="![](https://www.gif-vif.com/media/Hackerman.gif)"
 spotify="<a href=\"https://open.spotify.com/user/flxv095c8u53mlz4cjvagkgzl\" target=\"_blank\" rel=\"noopener noreferrer\"><picture><source media=\"(prefers-color-scheme: dark)\" srcset=\"https://spotify-readme-ofldzkxst-ngzhekai.vercel.app/api?rainbow=true\"><source media=\"(prefers-color-scheme: light)\" srcset=\"https://spotify-readme-ofldzkxst-ngzhekai.vercel.app/api?rainbow=true&theme=dark\"><img alt=\"Shows an real-time spotify now-playing widget white-theme in light mode and dark-theme in dark mode.\" src=\"https://spotify-readme-ofldzkxst-ngzhekai.vercel.app/api?rainbow=true\"></picture></a>"
+github_stats="<table><tr><td><picture><source media=\"(prefers-color-scheme: dark)\" srcset=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api/top-langs/?username=ngzhekai&layout=compact&theme=material-palenight&bg_color=00000000&hide_border=true&custom_title=Most+Used+Programming+Languages\"><source media=\"(prefers-color-scheme: light)\" srcset=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api/top-langs/?username=ngzhekai&layout=compact&theme=buefy&hide_border=true&custom_title=Most+Used+Programming+Languages\"><img alt=\"GitHub Top Language stats\" src=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api/top-langs/?username=ngzhekai&layout=compact&theme=material-palenight&bg_color=00000000&hide_border=true&custom_title=Most+Used+Programming+Languages\"></picture></td><td><picture><source media=\"(prefers-color-scheme: dark)\" srcset=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api?username=ngzhekai&show_icons=true&count_private=true&theme=material-palenight&bg_color=00000000&hide_border=true&line_height=33&custom_title=Zhe+Kai's+GitHub+Statistics\"><source media=\"(prefers-color-scheme: light)\" srcset=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api?username=ngzhekai&show_icons=true&count_private=true&theme=buefy&hide_border=true&line_height=33&custom_title=Zhe+Kai's+GitHub+Statistics\"><img alt=\"GitHub Stats\" src=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api?username=ngzhekai&show_icons=true&count_private=true&theme=material-palenight&bg_color=00000000&hide_border=true&line_height=33&custom_title=Zhe+Kai's+GitHub+Statistics\"></picture></td></tr></table>"
 
 echo $quote > README.md
 echo $note >> README.md
@@ -36,15 +35,11 @@ echo $title >> README.md
 echo $blank_space >> README.md
 echo $output >> README.md
 echo $blank_space >> README.md
-echo $stats >> README.md
-echo $stats_1 >> README.md
+echo $github_stats >> README.md
 echo $blank_space >> README.md
 echo $status >> README.md
 echo $blank_space >> README.md
 echo $spotify >> README.md
-echo $blank_space >> README.md
-echo $status_1 >> README.md
-echo $status_2 >> README.md
 echo $blank_space >> README.md
 echo $stats_2 >> README.md
 echo $blank_space >> README.md
