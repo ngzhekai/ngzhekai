eb158b47e7e30a6e0ee06e5229643387424c625e
panonFire <ngzhekai@gmail.com>
2023-03-02 14:18:59 +0800

    (change) update URL
    use Dracula Theme for GitHub readme stats

--- update_readme.sh
+++ update_readme.sh
@@ -26,7 +26,32 @@ status="### My Status [自分の情報]✅"
 stats_2="![](https://www.codewars.com/users/Giruba/badges/large)"
 stats_3="![](https://www.gif-vif.com/media/Hackerman.gif)"
 spotify="<a href=\"https://open.spotify.com/user/flxv095c8u53mlz4cjvagkgzl\" target=\"_blank\" rel=\"noopener noreferrer\"><picture><source media=\"(prefers-color-scheme: dark)\" srcset=\"https://spotify-readme-ofldzkxst-ngzhekai.vercel.app/api?rainbow=true\"><source media=\"(prefers-color-scheme: light)\" srcset=\"https://spotify-readme-ofldzkxst-ngzhekai.vercel.app/api?rainbow=true&theme=dark\"><img alt=\"Shows an real-time spotify now-playing widget white-theme in light mode and dark-theme in dark mode.\" src=\"https://spotify-readme-ofldzkxst-ngzhekai.vercel.app/api?rainbow=true\"></picture></a>"
-github_stats="<table><tr><td><picture><source media=\"(prefers-color-scheme: dark)\" srcset=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api/top-langs/?username=ngzhekai&layout=compact&theme=material-palenight&bg_color=00000000&hide_border=true&custom_title=Most+Used+Programming+Languages\"><source media=\"(prefers-color-scheme: light)\" srcset=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api/top-langs/?username=ngzhekai&layout=compact&theme=buefy&hide_border=true&custom_title=Most+Used+Programming+Languages\"><img alt=\"GitHub Top Language stats\" src=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api/top-langs/?username=ngzhekai&layout=compact&theme=material-palenight&bg_color=00000000&hide_border=true&custom_title=Most+Used+Programming+Languages\"></picture></td><td><picture><source media=\"(prefers-color-scheme: dark)\" srcset=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api?username=ngzhekai&show_icons=true&count_private=true&theme=material-palenight&bg_color=00000000&hide_border=true&line_height=33&custom_title=Zhe+Kai's+GitHub+Statistics\"><source media=\"(prefers-color-scheme: light)\" srcset=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api?username=ngzhekai&show_icons=true&count_private=true&theme=buefy&hide_border=true&line_height=33&custom_title=Zhe+Kai's+GitHub+Statistics\"><img alt=\"GitHub Stats\" src=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api?username=ngzhekai&show_icons=true&count_private=true&theme=material-palenight&bg_color=00000000&hide_border=true&line_height=33&custom_title=Zhe+Kai's+GitHub+Statistics\"></picture></td></tr></table>"
+github_stats="
+<table>
+  <tr>
+    <td>
+      <picture
+        ><source media=\"(prefers-color-scheme: dark)\"
+        srcset=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api/top-langs/?username=ngzhekai&layout=compact&theme=dracula&bg_color=00000000&hide_border=true&custom_title=Most+Used+Programming+Languages\"><source
+        media=\"(prefers-color-scheme: light)\"
+        srcset=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api/top-langs/?username=ngzhekai&layout=compact&theme=dracula&bg_color=00000000&hide_border=true&custom_title=Most+Used+Programming+Languages\"><img
+        alt=\"GitHub Top Language stats\"
+        src=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api/top-langs/?username=ngzhekai&layout=compact&theme=dracula&bg_color=00000000&hide_border=true&custom_title=Most+Used+Programming+Languages\"></picture
+      >
+    </td>
+    <td>
+      <picture
+        ><source media=\"(prefers-color-scheme: dark)\"
+        srcset=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api?username=ngzhekai&show_icons=true&count_private=true&theme=dracula&bg_color=00000000&hide_border=true&line_height=33&custom_title=Zhe+Kai's+GitHub+Statistics\"><source
+        media=\"(prefers-color-scheme:
+        light)\"srcset=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api?username=ngzhekai&show_icons=true&count_private=true&theme=dracula&bg_color=00000000&hide_border=true&line_height=33&custom_title=Zhe+Kai's+GitHub+Statistics\"><img
+        alt=\"GitHub Stats\"
+        src=\"https://github-readme-stats-eniocvy92-ngzhekai.vercel.app/api?username=ngzhekai&show_icons=true&count_private=true&theme=dracula&bg_color=00000000&hide_border=true&line_height=33&custom_title=Zhe+Kai's+GitHub+Statistics\"></picture
+      >
+    </td>
+  </tr>
+</table>
+"
 
 echo $blank_space > README.md
 echo $line_space >> README.md
